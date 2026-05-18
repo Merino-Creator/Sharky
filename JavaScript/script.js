28e83026c1a3c6d8b7911bb4106138db5d99b145
Daniel <stuermer.dan137@gmail.com>
2026-05-18 10:59:02 +0200

    included character image

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,7 +6,8 @@ function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    character.src = 'img/';
+    character.src = "./assets/1.Sharkie/1.IDLE/1.png";
 
     ctx.drawImage(character, 20, 20, 50, 150);
+    
 }
