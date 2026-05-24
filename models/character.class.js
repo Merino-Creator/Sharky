971e5a5169af61d62a4475fcef96befa60c30087
Daniel <stuermer.dan137@gmail.com>
2026-05-24 22:48:24 +0200

    added functions to move character in any direction

--- models/character.class.js
+++ models/character.class.js
@@ -4,6 +4,7 @@ class Character extends MoveableObject {
     y = 160;
     height = 160;
     width = 120;
+    speed = 10;
     world;
 
     IMAGES_CHARACTER = [
@@ -13,7 +14,7 @@ class Character extends MoveableObject {
         './assets/1.Sharkie/3.Swim/4.png',
         './assets/1.Sharkie/3.Swim/5.png',
         './assets/1.Sharkie/3.Swim/6.png'
-        
+
     ];
 
     constructor() {
@@ -24,12 +25,42 @@ class Character extends MoveableObject {
     }
 
     animate() {
-        setInterval( () => {
-            let i = this.currentImage % this.IMAGES_CHARACTER.length;
-            let path = this.IMAGES_CHARACTER[i];
-            this.img = this.ImageCache[path];
-            this.currentImage++;
-        }, 255);
+
+        setInterval(() => {
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 30);
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+        }, 1000 / 30);
+
+        setInterval(() => {
+            if (this.world.keyboard.UP) {
+                this.y -= this.speed;
+            }
+        }, 1000 / 30);
+
+        setInterval(() => {
+            if (this.world.keyboard.DOWN) {
+                this.y += this.speed;
+            }
+        }, 1000 / 30);
+
+        setInterval(() => {
+
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                this.x += this.speed;
+
+                let i = this.currentImage % this.IMAGES_CHARACTER.length;
+                let path = this.IMAGES_CHARACTER[i];
+                this.img = this.ImageCache[path];
+                this.currentImage++;
+            }
+        }, 100);
     }
 
     jump() {
