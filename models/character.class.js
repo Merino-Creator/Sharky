e44476b0e71824d24a60e1c983fa6c98bb00390f
Daniel <stuermer.dan137@gmail.com>
2026-05-24 16:13:38 +0200

    exchanged character graphics from idle to swim animation

--- models/character.class.js
+++ models/character.class.js
@@ -5,29 +5,18 @@ class Character extends MoveableObject {
     height = 160;
     width = 120;
     IMAGES_CHARACTER = [
-        './assets/1.Sharkie/1.IDLE/1.png',
-        './assets/1.Sharkie/1.IDLE/2.png',
-        './assets/1.Sharkie/1.IDLE/3.png',
-        './assets/1.Sharkie/1.IDLE/4.png',
-        './assets/1.Sharkie/1.IDLE/5.png',
-        './assets/1.Sharkie/1.IDLE/6.png',
-        './assets/1.Sharkie/1.IDLE/7.png',
-        './assets/1.Sharkie/1.IDLE/8.png',
-        './assets/1.Sharkie/1.IDLE/9.png',
-        './assets/1.Sharkie/1.IDLE/10.png',
-        './assets/1.Sharkie/1.IDLE/11.png',
-        './assets/1.Sharkie/1.IDLE/12.png',
-        './assets/1.Sharkie/1.IDLE/13.png',
-        './assets/1.Sharkie/1.IDLE/14.png',
-        './assets/1.Sharkie/1.IDLE/15.png',
-        './assets/1.Sharkie/1.IDLE/16.png',
-        './assets/1.Sharkie/1.IDLE/17.png',
-        './assets/1.Sharkie/1.IDLE/18.png'
+        './assets/1.Sharkie/3.Swim/1.png',
+        './assets/1.Sharkie/3.Swim/2.png',
+        './assets/1.Sharkie/3.Swim/3.png',
+        './assets/1.Sharkie/3.Swim/4.png',
+        './assets/1.Sharkie/3.Swim/5.png',
+        './assets/1.Sharkie/3.Swim/6.png'
+        
     ];
     currentImage = 0;
 
     constructor() {
-        super().loadImage('./assets/1.Sharkie/1.IDLE/1.png');
+        super().loadImage('./assets/1.Sharkie/3.Swim/1.png');
         this.loadImages(this.IMAGES_CHARACTER);
 
         this.animate();
