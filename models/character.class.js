47e08b1bab0e01fc6b78555ce3fcbc14ee3b00bd
Daniel <stuermer.dan137@gmail.com>
2026-05-24 15:47:53 +0200

    added imageCache for character graphics and its loading function

--- models/character.class.js
+++ models/character.class.js
@@ -7,6 +7,26 @@ class Character extends MoveableObject {
     
     constructor() {
         super().loadImage('./assets/1.Sharkie/1.IDLE/1.png');
+        this.loadImages([
+            './assets/1.Sharkie/1.IDLE/1.png',
+            './assets/1.Sharkie/1.IDLE/2.png',
+            './assets/1.Sharkie/1.IDLE/3.png',
+            './assets/1.Sharkie/1.IDLE/4.png',
+            './assets/1.Sharkie/1.IDLE/5.png',
+            './assets/1.Sharkie/1.IDLE/6.png',
+            './assets/1.Sharkie/1.IDLE/7.png',
+            './assets/1.Sharkie/1.IDLE/8.png',
+            './assets/1.Sharkie/1.IDLE/9.png',
+            './assets/1.Sharkie/1.IDLE/10.png',
+            './assets/1.Sharkie/1.IDLE/11.png',
+            './assets/1.Sharkie/1.IDLE/12.png',
+            './assets/1.Sharkie/1.IDLE/13.png',
+            './assets/1.Sharkie/1.IDLE/14.png',
+            './assets/1.Sharkie/1.IDLE/15.png',
+            './assets/1.Sharkie/1.IDLE/16.png',
+            './assets/1.Sharkie/1.IDLE/17.png',
+            './assets/1.Sharkie/1.IDLE/18.png'
+        ]);
     }
 
     jump() {
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -3,17 +3,27 @@ class MoveableObject {
     height = 80;
     width = 80;
 
+    ImageCache = {};
+
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
+    loadImages(grr) {
+        grr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.ImageCache[path] = path;
+        });
+    }
+
     moveRight() {
         console.log('moving right');
-        
+
     }
 
     moveLeft() {
-        
+
     }
 }
