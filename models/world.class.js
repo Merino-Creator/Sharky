d887a919218de7551e019045dfae05ff9123eff7
Daniel <stuermer.dan137@gmail.com>
2026-05-24 23:39:57 +0200

    repeated background manually

--- models/world.class.js
+++ models/world.class.js
@@ -17,10 +17,28 @@ class World {
     ];
 
     backgroundObjects = [
-        new BackgroundObject('assets/3. Background/Layers/5. Water/D.png', 0),
-        new BackgroundObject('assets/3. Background/Layers/3.Fondo 1/D.png', 0),
-        new BackgroundObject('assets/3. Background/Layers/4.Fondo 2/D.png', 0),
-        new BackgroundObject('assets/3. Background/Layers/2. Floor/D.png', 0),
+        new BackgroundObject('assets/3. Background/Layers/5. Water/D2.png', -720),
+        new BackgroundObject('assets/3. Background/Layers/3.Fondo 1/D2.png', -720),
+        new BackgroundObject('assets/3. Background/Layers/4.Fondo 2/D2.png', -720),
+        new BackgroundObject('assets/3. Background/Layers/2. Floor/D2.png', -720),
+
+        new BackgroundObject('assets/3. Background/Layers/5. Water/D1.png', 0),
+        new BackgroundObject('assets/3. Background/Layers/3.Fondo 1/D1.png', 0),
+        new BackgroundObject('assets/3. Background/Layers/4.Fondo 2/D1.png', 0),
+        new BackgroundObject('assets/3. Background/Layers/2. Floor/D1.png', 0),
+        new BackgroundObject('assets/3. Background/Layers/5. Water/D2.png', 720),
+        new BackgroundObject('assets/3. Background/Layers/3.Fondo 1/D2.png', 720),
+        new BackgroundObject('assets/3. Background/Layers/4.Fondo 2/D2.png', 720),
+        new BackgroundObject('assets/3. Background/Layers/2. Floor/D2.png', 720),
+
+        new BackgroundObject('assets/3. Background/Layers/5. Water/D1.png', 720*2),
+        new BackgroundObject('assets/3. Background/Layers/3.Fondo 1/D1.png', 720*2),
+        new BackgroundObject('assets/3. Background/Layers/4.Fondo 2/D1.png', 720*2),
+        new BackgroundObject('assets/3. Background/Layers/2. Floor/D1.png', 720*2),
+        new BackgroundObject('assets/3. Background/Layers/5. Water/D2.png', 720*3),
+        new BackgroundObject('assets/3. Background/Layers/3.Fondo 1/D2.png', 720*3),
+        new BackgroundObject('assets/3. Background/Layers/4.Fondo 2/D2.png', 720*3),
+        new BackgroundObject('assets/3. Background/Layers/2. Floor/D2.png', 720*3),
     ];
 
     canvas;
