f570c4a71e8fa380fcc1a3832ffcec2f14b72516
Daniel <stuermer.dan137@gmail.com>
2026-05-25 18:03:21 +0200

    extended enemies spawning area to full width (2000)

--- models/jellyfish.class.js
+++ models/jellyfish.class.js
@@ -11,7 +11,7 @@ class Jellyfish extends MoveableObject {
         super().loadImage(this.IMAGES_JELLY[0]);
         this.loadImages(this.IMAGES_JELLY);
 
-        this.x = 200 + Math.random() * 500;
+        this.x = 200 + Math.random() * 2000;
         let topZone = Math.random() < 0.5;
         if (topZone) {
             this.y = Math.random() * 160;
--- models/pufferFish.class.js
+++ models/pufferFish.class.js
@@ -12,7 +12,7 @@ class Puffer extends MoveableObject {
         super().loadImage('assets/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
         this.loadImages(this.IMAGES_ENEMY);
 
-        this.x = 200 + Math.random() * 500;
+        this.x = 200 + Math.random() * 2000;
         this.y = 240;
 
         this.speed = 0.2 + Math.random() * 0.25;
