f9ef5058b57160975f40aa2aaa5f7305db9265ed
Daniel <stuermer.dan137@gmail.com>
2026-06-16 18:30:25 +0200

    finished organizing endboss.class

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -12,6 +12,11 @@ class Endboss extends MoveableObject {
     firstContact = false;
     energy = 1000;
     damage = 50;
+    isAttacking = false;
+    introFinished = false;
+    startX = 3800;
+    frameCounter = 0;
+    deadFrame = 0;
 
     BOSS_ATTACK_AUDIO = new Audio('./assets/8.Audio/boss-attack.mp3');
     BOSS_DIES_AUDIO = new Audio('./assets/8.Audio/boss-dies.mp3');
@@ -63,91 +68,18 @@ class Endboss extends MoveableObject {
 
     /**
      * Starts all animation and movement intervals for the Endboss.
-     * Plays the intro animation once on first contact, then alternates between
-     * floating and attacking every 1600ms. During an attack the boss moves
-     * 200px toward the character and smoothly returns to its start position afterward.
      * Stores all interval IDs for later cleanup.
      */
     animate() {
-        let isAttacking = false;
-        let introFinished = false;
-        let startX = this.x;
-        let frameCounter = 0;
-        let deadFrame = 0;
-
         let bossAttackTriggerId = setInterval(() => {
-            if (gamePaused) return;
-            if (this.firstContact && introFinished) {
-                isAttacking = true;
-                this.BOSS_ATTACK_AUDIO.play();
-                registerAudio(this.BOSS_ATTACK_AUDIO);
-
-                let attackId = setInterval(() => {
-                    if (gamePaused) return;
-                    if (this.x > startX - 200) {
-                        this.x -= 5;
-                    } else {
-                        clearInterval(attackId);
-                    }
-                }, 1000 / 30);
-
-                setTimeout(() => {
-                    isAttacking = false;
-                    clearInterval(attackId);
-
-                    let returnId = setInterval(() => {
-                        if (gamePaused) return;
-                        if (this.x < startX) {
-                            this.x += 5;
-                        } else {
-                            clearInterval(returnId);
-                        }
-                    }, 1000 / 30);
-                }, 6 * 100);
-            }
+            this.triggerAttack();
         }, 1600);
         intervalIds.push(bossAttackTriggerId);
 
         let bossAnimateId = setInterval(() => {
             if (gamePaused) return;
-            frameCounter++;
-
-            if (this.isDead()) {
-                if (frameCounter % 2 === 0) {
-                    if (deadFrame < this.IMAGES_BOSS_DEAD.length) {
-                        this.img = this.ImageCache[this.IMAGES_BOSS_DEAD[deadFrame]];
-                        deadFrame++;
-                        if (deadFrame === 1) {
-                            setTimeout(() => {
-                                this.BOSS_DIES_AUDIO.play();
-                                setTimeout(() => {
-                                    this.BOSS_DIES_AUDIO.pause();
-                                    this.BOSS_DIES_AUDIO.currentTime = 0;
-                                }, 1000);
-                            }, 100);
-                        }
-                    } else {
-                        this.img = this.ImageCache[this.IMAGES_BOSS_DEAD[this.IMAGES_BOSS_DEAD.length - 1]];
-                    }
-                }
-            } else if (this.bossIsHurt()) {
-                if (frameCounter % 1 === 0)
-                    this.playAnimation(this.IMAGES_BOSS_HURT);
-            } else if (!this.firstContact) {
-                if (frameCounter % 2 === 0)
-                    this.playAnimation(this.IMAGES_BOSS_FLOAT);
-            } else if (this.firstContact && this.currentImage < this.IMAGES_BOSS_INTRO.length) {
-                if (frameCounter % 2 === 0)
-                    this.playAnimation(this.IMAGES_BOSS_INTRO);
-            } else if (isAttacking) {
-                introFinished = true;
-                if (frameCounter % 2 === 0)
-                    this.playAnimation(this.IMAGES_BOSS_ATTACK);
-            } else {
-                introFinished = true;
-                if (frameCounter % 2 === 0)
-                    this.playAnimation(this.IMAGES_BOSS_FLOAT);
-            }
+            this.frameCounter++;
+            this.checkAnimation();
         }, 100);
         intervalIds.push(bossAnimateId);
 
@@ -156,9 +88,124 @@ class Endboss extends MoveableObject {
             if (world && world.character.x > 3050 && !this.firstContact) {
                 this.firstContact = true;
                 this.currentImage = 0;
-                startX = this.x;
+                this.startX = this.x;
             }
         }, 200);
         intervalIds.push(bossContactId);
+    }
+
+    /**
+     * Determines and plays the correct animation based on the current Endboss state.
+     * Priority order: dead > hurt > float > intro > attacking > floating.
+     */
+    checkAnimation() {
+        if (this.isDead()) {
+            this.playDeadAnimation();
+        } else if (this.bossIsHurt()) {
+            this.playHurtAnimation();
+        } else if (this.isAttacking) {
+            this.playAttackAnimation();
+        } else if (this.firstContact && this.currentImage < this.IMAGES_BOSS_INTRO.length) {
+            this.playIntroAnimation();
+        } else {
+            if (this.firstContact) this.introFinished = true;
+            this.playFloatAnimation();
+        }
+    }
+
+    /**
+     * Triggers the attack sequence if first contact has been made and intro is finished.
+     * Moves the boss toward the character and returns to start position afterward.
+     */
+    triggerAttack() {
+        if (gamePaused) return;
+        if (this.firstContact && this.introFinished) {
+            this.isAttacking = true;
+            this.BOSS_ATTACK_AUDIO.play();
+            registerAudio(this.BOSS_ATTACK_AUDIO);
+
+            let attackId = setInterval(() => {
+                if (gamePaused) return;
+                this.x -= 3;
+            }, 1000 / 30);
+
+            setTimeout(() => {
+                this.attackAnimationXPosition(attackId);
+            }, 6 * 100);
+        }
+    }
+
+    /**
+     * Stops the attack movement and smoothly returns the boss to its start position.
+     * @param {number} attackId - The interval ID of the attack movement to clear.
+     */
+    attackAnimationXPosition(attackId) {
+        this.isAttacking = false;
+        clearInterval(attackId);
+
+        let returnId = setInterval(() => {
+            if (gamePaused) return;
+            if (this.x < this.startX) {
+                this.x += 3;
+            } else {
+                clearInterval(returnId);
+            }
+        }, 1000 / 30);
+    }
+
+    /**
+     * Plays the dead animation and triggers the death sound once.
+     */
+    playDeadAnimation() {
+        if (this.frameCounter % 2 === 0) {
+            if (this.deadFrame < this.IMAGES_BOSS_DEAD.length) {
+                this.img = this.ImageCache[this.IMAGES_BOSS_DEAD[this.deadFrame]];
+                this.deadFrame++;
+                if (this.deadFrame === 1) {
+                    setTimeout(() => {
+                        this.BOSS_DIES_AUDIO.play();
+                        setTimeout(() => {
+                            this.BOSS_DIES_AUDIO.pause();
+                            this.BOSS_DIES_AUDIO.currentTime = 0;
+                        }, 1000);
+                    }, 100);
+                }
+            } else {
+                this.img = this.ImageCache[this.IMAGES_BOSS_DEAD[this.IMAGES_BOSS_DEAD.length - 1]];
+            }
+        }
+    }
+
+    /**
+     * Plays the hurt animation.
+     */
+    playHurtAnimation() {
+        if (this.frameCounter % 1 === 0)
+            this.playAnimation(this.IMAGES_BOSS_HURT);
+    }
+
+    /**
+     * Plays the float animation before first contact.
+     */
+    playFloatAnimation() {
+        if (this.frameCounter % 2 === 0)
+            this.playAnimation(this.IMAGES_BOSS_FLOAT);
+    }
+
+    /**
+     * Plays the intro animation once on first contact.
+     */
+    playIntroAnimation() {
+        if (this.frameCounter % 2 === 0)
+            this.playAnimation(this.IMAGES_BOSS_INTRO);
+    }
+
+    /**
+     * Plays the attack animation and marks the intro as finished.
+     */
+    playAttackAnimation() {
+        this.introFinished = true;
+        if (this.frameCounter % 2 === 0)
+            this.playAnimation(this.IMAGES_BOSS_ATTACK);
     }
 }
