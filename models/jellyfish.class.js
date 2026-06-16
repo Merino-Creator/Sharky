4a8822360a49b8c4a660080238e0727bd80902c6
Daniel <stuermer.dan137@gmail.com>
2026-06-16 17:52:51 +0200

    created jellyfish json file // organsized jellyfish class

--- models/jellyfish.class.js
+++ models/jellyfish.class.js
@@ -12,38 +12,48 @@ class Jellyfish extends MoveableObject {
     JELLY_AGRESSIVE_AUDIO = new Audio('./assets/8.Audio/jellyfish-agressive.mp3');
     JELLY_DEAD_AUDIO = new Audio('./assets/8.Audio/jellyfish-dead.mp3');
 
-    IMAGES_JELLY = [
-        './assets/2.Enemy/2 Jelly fish/Regular damage/Lila 1.png',
-        './assets/2.Enemy/2 Jelly fish/Regular damage/Lila 2.png',
-        './assets/2.Enemy/2 Jelly fish/Regular damage/Lila 3.png',
-        './assets/2.Enemy/2 Jelly fish/Regular damage/Lila 4.png'
-    ];
-
-    IMAGES_JELLY_DEAD = [
-        './assets/2.Enemy/2 Jelly fish/Dead/Lila/L1.png',
-        './assets/2.Enemy/2 Jelly fish/Dead/Lila/L2.png',
-        './assets/2.Enemy/2 Jelly fish/Dead/Lila/L3.png',
-        './assets/2.Enemy/2 Jelly fish/Dead/Lila/L4.png'
-    ];
-
-    IMAGES_JELLY_AGRESSIVE = [
-        './assets/2.Enemy/2 Jelly fish/Súper dangerous/Pink 1.png',
-        './assets/2.Enemy/2 Jelly fish/Súper dangerous/Pink 2.png',
-        './assets/2.Enemy/2 Jelly fish/Súper dangerous/Pink 3.png',
-        './assets/2.Enemy/2 Jelly fish/Súper dangerous/Pink 4.png'
-    ];
-
     /**
      * Creates a new Jellyfish instance with a random position in either
      * the top zone (0-160) or bottom zone (300-400) of the canvas.
      */
     constructor() {
-        super().loadImage(this.IMAGES_JELLY[0]);
+        super().loadImage('./assets/2.Enemy/2 Jelly fish/Regular damage/Lila 1.png');
+        this.x = this.generateX();
+        this.checkYPosition();
+        this.speed = 1 + Math.random() * 0.25;
+        this.fetchJellyfishImages();
+    }
+
+    /**
+     * Fetches all jellyfish animation image paths from the JSON file,
+     * assigns them to the corresponding image arrays and starts the animation.
+     */
+    async fetchJellyfishImages() {
+        let response = await fetch('./jsons/jellyfish-images.json');
+        let images = await response.json();
+
+        this.IMAGES_JELLY = images.swim;
+        this.IMAGES_JELLY_DEAD = images.dead;
+        this.IMAGES_JELLY_AGRESSIVE = images.agressive;
+
+        this.loadJellyfishImages();
+        this.animate();
+    }
+
+    /**
+     * Preloads all jellyfish animation images into the ImageCache.
+     */
+    loadJellyfishImages() {
         this.loadImages(this.IMAGES_JELLY);
         this.loadImages(this.IMAGES_JELLY_DEAD);
         this.loadImages(this.IMAGES_JELLY_AGRESSIVE);
+    }
 
-        this.x = this.generateX();
+    /**
+     * Sets the y position and movement boundaries for the Jellyfish
+     * randomly in either the top zone or bottom zone of the canvas.
+     */
+    checkYPosition() {
         let topZone = Math.random() < 0.5;
         if (topZone) {
             this.y = Math.random() * 160;
@@ -54,9 +64,6 @@ class Jellyfish extends MoveableObject {
             this.yMin = 300;
             this.yMax = 400;
         }
-
-        this.speed = 1 + Math.random() * 0.25;
-        this.animate();
     }
 
     /**
@@ -79,46 +86,73 @@ class Jellyfish extends MoveableObject {
 
     /**
      * Starts the animation and movement intervals for the Jellyfish.
-     * Switches to aggressive mode when energy reaches 25.
      * Stores all interval IDs for later cleanup.
      */
     animate() {
-        let agressiveSoundPlaying = false;
-        let deadSoundPlaying = false;
+        this.agressiveSoundPlaying = false;
+        this.deadSoundPlaying = false;
 
         let jellyAnimateId = setInterval(() => {
-            if (this.isDead()) {
-                this.playAnimation(this.IMAGES_JELLY_DEAD);
-
-                if (!deadSoundPlaying) {
-                    deadSoundPlaying = true;
-                    this.JELLY_DEAD_AUDIO.play();
-                    registerAudio(this.JELLY_DEAD_AUDIO);
-                    setTimeout(() => {
-                        this.JELLY_DEAD_AUDIO.pause();
-                        this.JELLY_DEAD_AUDIO.currentTime = 0;
-                    }, 1000);
-                }
-            } else if (this.energy == 25) {
-                this.playAnimation(this.IMAGES_JELLY_AGRESSIVE);
-                this.agressiveMode();
-
-                if (!agressiveSoundPlaying) {
-                    agressiveSoundPlaying = true;
-                    this.JELLY_AGRESSIVE_AUDIO.play();
-                    registerAudio(this.JELLY_AGRESSIVE_AUDIO);
-                    setTimeout(() => {
-                        this.JELLY_AGRESSIVE_AUDIO.pause();
-                        this.JELLY_AGRESSIVE_AUDIO.currentTime = 0;
-                    }, 1000);
-                }
-            } else {
-                this.playAnimation(this.IMAGES_JELLY);
-            }
+            this.checkAnimation();
         }, 250);
 
         intervalIds.push(jellyAnimateId);
         this.moveUpDown();
+    }
+
+    /**
+     * Determines and plays the correct animation based on the current Jellyfish state.
+     * Priority order: dead > aggressive > swim.
+     */
+    checkAnimation() {
+        if (gamePaused) return;
+        if (this.isDead()) {
+            this.playDeadAnimation();
+        } else if (this.energy == 25) {
+            this.playAgressiveAnimation();
+        } else {
+            this.playSwimAnimation();
+        }
+    }
+
+    /**
+     * Plays the dead animation and the death sound once.
+     */
+    playDeadAnimation() {
+        this.playAnimation(this.IMAGES_JELLY_DEAD);
+        if (!this.deadSoundPlaying) {
+            this.deadSoundPlaying = true;
+            this.JELLY_DEAD_AUDIO.play();
+            registerAudio(this.JELLY_DEAD_AUDIO);
+            setTimeout(() => {
+                this.JELLY_DEAD_AUDIO.pause();
+                this.JELLY_DEAD_AUDIO.currentTime = 0;
+            }, 1000);
+        }
+    }
+
+    /**
+     * Plays the aggressive animation, activates aggressive mode and plays the aggressive sound once.
+     */
+    playAgressiveAnimation() {
+        this.playAnimation(this.IMAGES_JELLY_AGRESSIVE);
+        this.agressiveMode();
+        if (!this.agressiveSoundPlaying) {
+            this.agressiveSoundPlaying = true;
+            this.JELLY_AGRESSIVE_AUDIO.play();
+            registerAudio(this.JELLY_AGRESSIVE_AUDIO);
+            setTimeout(() => {
+                this.JELLY_AGRESSIVE_AUDIO.pause();
+                this.JELLY_AGRESSIVE_AUDIO.currentTime = 0;
+            }, 1000);
+        }
+    }
+
+    /**
+     * Plays the standard swim animation.
+     */
+    playSwimAnimation() {
+        this.playAnimation(this.IMAGES_JELLY);
     }
 
     /**
